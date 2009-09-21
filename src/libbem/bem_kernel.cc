935762a8d885051b573790f66ad01b2557dd8c94
azacharo <azacharo@d6d68596-90e5-4b0b-b9dc-454a62485beb>
2009-09-21 11:48:41 +0000

    added kernel for d(wave number)/d(measurements)

--- src/libbem/bem_kernel.cc
+++ src/libbem/bem_kernel.cc
@@ -29,12 +29,12 @@ CVector BEM_Kernel_Helmholtz::Calculate (BEM_Element *el, Point2D &loc, const Po
 
   double xp = load[0], yp = load[1], zp = load[2];
   xpxq=xq-xp; ypyq=yq-yp; zpzq=zq-zp;
-  rpq2=1./(xpxq*xpxq+ypyq*ypyq+zpzq*zpzq);
-  rpq1=sqrt(rpq2);
+  rpq2=1./(xpxq*xpxq+ypyq*ypyq+zpzq*zpzq);	// 1/|r-r'|^2
+  rpq1=sqrt(rpq2);							// 1/|r-r'|
   
-  krpq1= -k/rpq1;
-  e_aux=exp(krpq1);
-  k_aux=-(k+rpq1)*e_aux*pi4*rpq2;
+  krpq1= -k/rpq1;							// - k *|r-r'|
+  e_aux=exp(krpq1);							// exp( - k *|r-r'|)
+  k_aux=-(k+rpq1)*e_aux*pi4*rpq2;			
 
   // kernel1 ->  kern_tab[0]; kern_tab[1]; kern_tab[2]; 
   kern_tab[0] = k_aux*xpxq;
@@ -98,5 +98,53 @@ CVector BEM_Kernel_Domega::Calculate (BEM_Element *el, Point2D &loc, const Point
   //kern_tab[3] = e_aux*rpq1*pi4;
   kern_tab[3] = 0.0;
 
+  return kern_tab;
+}
+
+
+
+CVector BEM_Kernel_Dk::Calculate (BEM_Element *el, Point2D &loc, const Point3D &load)
+{
+  
+  //==================================================================
+  //  The kernel is the derivative of the Green's function with respect
+  //  to the wavenumber.
+  //  Diffusion equation
+  //==================================================================
+  int iic; 
+  double xq, yq, zq, rpq1, rpq2, xpxq, ypyq, zpzq;
+  complex krpq1, k_aux, e_aux,  dk_aux;
+  static RVector shapf;
+  static CVector kern_tab(4);
+
+  // local coordinate system
+  xq = 0.;    yq = 0.;    zq = 0.;
+  shapf = el->ShapeF (loc);
+  Point *nd = el->Surface()->NodeList();
+
+  for(int ic=0; ic<el->nNode();++ic){
+	  iic = el->NodeIndex(ic);
+      xq += shapf[ic]*nd[iic][0];
+      yq += shapf[ic]*nd[iic][1];
+      zq += shapf[ic]*nd[iic][2];
+  }
+
+  double xp = load[0], yp = load[1], zp = load[2];
+  xpxq=xq-xp; ypyq=yq-yp; zpzq=zq-zp;
+  rpq2=1./(xpxq*xpxq+ypyq*ypyq+zpzq*zpzq);	// 1/|r-r'|^2
+  rpq1=sqrt(rpq2);							// 1/|r-r'|
+  
+  krpq1= -k/rpq1;							// - k *|r-r'|
+  e_aux=exp(krpq1);							// exp( - k *|r-r'|)
+ // k_aux=-(k+rpq1)*e_aux*pi4*rpq2;			// exp( - k *|r-r'|)* (1/4Pi)*{ }
+  dk_aux= k*e_aux*rpq1*pi4;   // check that
+  // kernel1 ->  kern_tab[0]; kern_tab[1]; kern_tab[2]; 
+  kern_tab[0] = dk_aux*xpxq;
+  kern_tab[1] = dk_aux*ypyq;
+  kern_tab[2] = dk_aux*zpzq;
+  
+  // the kernel2 -> kern_tab[3] 
+  kern_tab[3] = - e_aux*pi4;
+  
   return kern_tab;
 }
