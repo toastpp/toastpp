d860104a72eb4c1ea594024e247e06ded3d119e1
ucacmas <ucacmas@d6d68596-90e5-4b0b-b9dc-454a62485beb>
2010-02-18 21:21:54 +0000

    Added toastMeshOpt and toastMeshReorder functions

--- toast/trunk/src/matlab/toastJacobianCW.cc
+++ toast/trunk/src/matlab/toastJacobianCW.cc
@@ -29,9 +29,6 @@ using namespace toast;
 // =========================================================================
 // local prototypes
 
-//template<class T>
-//void ImageGradient (const IVector &dim, const RVector &size,
-//    const TVector<T> &im, TVector<T> *grad, const int *mask = 0);
 void Project (const QMMesh &mesh, int q, const RVector &phi,
     RVector &proj);
 void PMDF_mua (const RVector &dphi, const RVector &aphi, RVector &pmdf);
@@ -105,123 +102,8 @@ void CalcJacobian (QMMesh *mesh, Raster *raster,
 }                                                                              
 
 // ============================================================================
-
-#ifdef UNDEF
-template<class T>
-void ImageGradient (const IVector &gdim, const RVector &gsize,
-    const TVector<T> &im, TVector<T> *grad, const int *mask)
-{
-    // this should be done by Fourier transform
-    int x, y, z, idx, dim = gdim.Dim();
-    int nx = gdim[0], ny = gdim[1], nz = (dim >= 3 ? gdim[2]:1);
-    int n = nx*ny*nz;
-
-    // x gradient
-    double dx = gsize[0]/nx, ix = 1.0/dx, i2x = 0.5*ix;
-    TVector<T> &gradx = grad[0];
-    gradx.New (n);
-    for (z = idx = 0; z < nz; z++) {
-        for (y = 0; y < ny; y++) {
-	    for (x = 0; x < nx; x++) {
-	        bool bcnt   = (!mask || (mask[idx] >= 0));
-		bool bleft  = (x > 0 && (!mask || (mask[idx-1] >= 0)));
-		bool bright = (x < nx-1 && (!mask || (mask[idx+1] >= 0)));
-		if (bleft && bright) {
-		    gradx[idx] = (im[idx+1]-im[idx-1]) * i2x;
-		    INC_FLOPS_ADD(1);
-		    INC_FLOPS_MUL(1);
-		} else if (!bcnt) {
-		    gradx[idx] = 0.0;
-		} else if (bleft) {
-		    gradx[idx] = (im[idx]-im[idx-1]) * ix;
-		    INC_FLOPS_ADD(1);
-		    INC_FLOPS_MUL(1);
-		} else if (bright) {
-		    gradx[idx] = (im[idx+1]-im[idx]) * ix;
-		    INC_FLOPS_ADD(1);
-		    INC_FLOPS_MUL(1);
-		} else {
-		    gradx[idx] = 0.0;
-		}
-		idx++;
-	    }
-	}
-    }
-
-    // y gradient
-    double dy = gsize[1]/ny, iy = 1.0/dy, i2y = 0.5*iy;
-    TVector<T> &grady = grad[1];
-    grady.New(n);
-    for (z = idx = 0; z < nz; z++) {
-        for (y = 0; y < ny; y++) {
-	    for (x = 0; x < nx; x++) {
-	        bool bcnt  = (!mask || (mask[idx] >= 0));
-		bool bup   = (y > 0 && (!mask || (mask[idx-nx] >= 0)));
-		bool bdown = (y < ny-1 && (!mask || (mask[idx+nx] >= 0)));
-		if (bup && bdown) {
-		    grady[idx] = (im[idx+nx]-im[idx-nx]) * i2y;
-		    INC_FLOPS_ADD(1);
-		    INC_FLOPS_MUL(1);
-		} else if (!bcnt) {
-		    grady[idx] = 0.0;
-		} else if (bup) {
-		    grady[idx] = (im[idx]-im[idx-nx]) * iy;
-		    INC_FLOPS_ADD(1);
-		    INC_FLOPS_MUL(1);
-		} else if (bdown) {
-		    grady[idx] = (im[idx+nx]-im[idx]) * iy;
-		    INC_FLOPS_ADD(1);
-		    INC_FLOPS_MUL(1);
-		} else {
-		    grady[idx] = 0.0;
-		}
-		idx++;
-	    }
-	}
-    }
-    if (dim < 3) return;
-
-    // z gradient
-    double dz = gsize[2]/nz, iz = 1.0/dz, i2z = 0.5*iz;
-    int stridez = nx*ny;
-    TVector<T> &gradz = grad[2];
-    gradz.New(n);
-    for (z = idx = 0; z < nz; z++) {
-        for (y = 0; y < ny; y++) {
-	    for (x = 0; x < nx; x++) {
-	        bool bcnt = (!mask || (mask[idx] >= 0));
-		bool bfront = (z > 0 && !(mask || (mask[idx-stridez] >= 0)));
-		bool bback  = (z < nz-1 && (!mask || (mask[idx+stridez] >= 0)));
-	        if (bfront && bback) {
-		    gradz[idx] = (im[idx+stridez]-im[idx-stridez]) * i2z;
-		    INC_FLOPS_ADD(1);
-		    INC_FLOPS_MUL(1);
-		} else if (!bcnt) {
-		    gradz[idx] = 0.0;
-		} else if (bfront) {
-		    gradz[idx] = (im[idx]-im[idx-stridez]) * iz;
-		    INC_FLOPS_ADD(1);
-		    INC_FLOPS_MUL(1);
-		} else if (bback) {
-		    gradz[idx] = (im[idx+stridez]-im[idx]) * iz;
-		    INC_FLOPS_ADD(1);
-		    INC_FLOPS_MUL(1);
-		} else {
-		    gradz[idx] = 0.0;
-		}
-		idx++;
-	    }
-	}
-    }
-}
-// instantiation
-template void ImageGradient (const IVector &gdim, const RVector &gsize,
-    const RVector &im, RVector *grad, const int *mask);
-template void ImageGradient (const IVector &gdim, const RVector &gsize,
-    const CVector &im, CVector *grad, const int *mask);
-#endif
-
 // generate a projection from a field - complex case
+
 void Project (const QMMesh &mesh, int q, const RVector &phi,
     RVector &proj)
 {
