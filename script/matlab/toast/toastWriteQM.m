679788f76e6fd45798d36727bfca3274022ba167
ucacmas <mschweiger@users.noreply.github.com>
2010-03-09 12:42:01 +0000

    Fixed description for toastWriteQM

--- script/matlab/toast/toastWriteQM.m
+++ script/matlab/toast/toastWriteQM.m
@@ -4,14 +4,19 @@ function p = toastWriteQM
 % Synposis: toastWriteQM (qpos, mpos, linklist, fname)
 %     qpos:     vector of source positions (nq x dim)
 %     mpos:     vector of measurement positions (nm x dim)
-%     linklist: source-detector link information (nq x nm)
+%     linklist: source-detector link information (nm x nq)
 %     fname:    QM output file name
 %
 % qpos and mpos contain the boundary positions of the source and detector
 % locations.
 % linklist contains the connectivity information between sources and
-% detectors. It should be a sparse nq x nm matrix, where the nonzero
-% entries for column i contain the indices (1-based) of all detectors
-% containing data for source i.
+% detectors. It should be a sparse binary nm x nq matrix, where the nonzero
+% entries for column j contain the indices (1-based) of all detectors
+% containing data for source j.
+%
+% To create a fully populated link list (all sources connect to all
+% detectors), use
+%
+%   linklist = sparse(ones(nm,nq));
   
   
