0fc652619552e7b5802cb95ab14ac0a5702d458a
sprerapa <sprerapa@d6d68596-90e5-4b0b-b9dc-454a62485beb>
2011-05-11 14:04:46 +0000

    Added PNvarOrd which contains all the work done by Surya on pn approximation

--- toast/trunk/src/libvarPN3D/source.h
+++ toast/trunk/src/libvarPN3D/source.h
@@ -3,11 +3,15 @@
 
 #include "mathlib.h"
 #include "felib.h"
+#include "sphericalHarmonic_algebra.h"
+#include "pparse.h"
 
 typedef enum {
     SRCMODE_NEUMANN,
     SRCMODE_ISOTROPIC
 } SourceMode;
+
+void SelectSourceProfile (ParamParser &pp, int &qtype, double &qwidth, SourceMode &srctp);
 
 RVector QVec_Gaussian (const Mesh &mesh, const Point &cnt, double w,
     SourceMode mode);
@@ -17,4 +21,22 @@ RVector QVec_Cosine (const Mesh &mesh, const Point &cnt, double w,
 
 RVector QVec_Point (const Mesh &mesh, const Point &cnt, SourceMode mode);
 
+/* Computes the source vectors for all the boundary sources when a QM file has been specified
+*/
+void genmat_toastsource(const IVector& sphOrder, const IVector& node_angN, const IVector& offset, RCompRowMatrix* & Source, const Mesh& mesh, const RCompRowMatrix qvec, const int ns, RVector& dirVec, const int srctp, const RDenseMatrix& pts, const RVector& wts, const RCompRowMatrix& b1, RDenseMatrix* &Ylm);
+
+/** Computes source vectors for point sources on the boundary 
+**/
+void genmat_source(const IVector& sphOrder, const IVector& node_angN, const IVector& offset, RCompRowMatrix* & Source, const Mesh& mesh,  const IVector& Nsource, const int ns, RVector& dirVec, const int srctp, const RDenseMatrix& pts, const RVector& wts, const RCompRowMatrix& b1, RDenseMatrix* &Ylm);
+
+/** Computes source vectors for point sources in the interior 
+**/
+void genmat_intsource(const IVector& sphOrder, const IVector& node_angN, const IVector& offset, RCompRowMatrix* & Source, const Mesh& mesh,  const IVector& Nsource, const int ns, RVector& dirVec, const int srctp, const RDenseMatrix& pts, const RVector& wts, RDenseMatrix* &Ylm, const RVector &delta);
+
+/** When used in conjunction with genmat_source function with point sources, this function simulates an uncollided source 
+**/
+void genmat_intsourcelaser(const IVector& sphOrder, const IVector& node_angN, const IVector& offset, RCompRowMatrix* &Source, Mesh& mesh, const IVector& Nsource, const int ns, RVector& dirVec, const RDenseMatrix& pts, const RVector& wts, const RVector &mua, const RVector &mus, const RVector &delta, const double g);
+
+
+//
 #endif // !__SOURCE_H
--- toast/trunk/src/libvarPN3D/toast.h
+++ toast/trunk/src/libvarPN3D/toast.h
@@ -1,1 +1,6 @@
-// empty file
+#include "mathlib.h"
+/** Thresholds and shrinks a real dense matrix to give a real sparse matrix
+* NOTE!! The threshold is set to 1e-15
+**/
+RCompRowMatrix shrink(const RDenseMatrix &dnsmat);
+
