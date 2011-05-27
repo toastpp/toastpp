6cc9723e23c4b93b51dde0251d527063cbbf2e46
ucacmas <mschweiger@users.noreply.github.com>
2011-05-27 15:24:35 +0000

    Changed slu_Cnames.h so that toast-specific BLAS and LAPACK functions are used

--- numerics/SuperLU_4.1/SRC/slu_Cnames.h
+++ numerics/SuperLU_4.1/SRC/slu_Cnames.h
@@ -20,6 +20,12 @@
 #define __SUPERLU_CNAMES
 
 
+
+// Force use of TOAST version of BLAS libraries (32-bit integer)
+#define TOAST_BLAS_32INT
+
+
+
 #define ADD_       0
 #define ADD__      1
 #define NOCHANGE   2
@@ -52,6 +58,91 @@
 #define F77_CALL_C  ADD_
 #endif
 
+#ifdef TOAST_BLAS_32INT
+
+/* BLAS */
+#define sswap_    toast_sswap_
+#define saxpy_    toast_saxpy_
+#define sasum_    toast_sasum_
+#define isamax_   toast_isamax_
+#define scopy_    toast_scopy_
+#define sscal_    toast_sscal_
+#define sger_     toast_sger_
+#define snrm2_    toast_snrm2_
+#define ssymv_    toast_ssymv_
+#define sdot_     toast_sdot_
+#define saxpy_    toast_saxpy_
+#define ssyr2_    toast_ssyr2_
+#define srot_     toast_srot_
+#define sgemv_    toast_sgemv_
+#define strsv_    toast_strsv_
+#define sgemm_    toast_sgemm_
+#define strsm_    toast_strsm_
+
+#define dswap_    toast_dswap_
+#define daxpy_    toast_daxpy_
+#define dasum_    toast_dasum_
+#define idamax_   toast_idamax_
+#define dcopy_    toast_dcopy_
+#define dscal_    toast_dscal_
+#define dger_     toast_dger_
+#define dnrm2_    toast_dnrm2_
+#define dsymv_    toast_dsymv_
+#define ddot_     toast_ddot_
+#define dsyr2_    toast_dsyr2_
+#define drot_     toast_drot_
+#define dgemv_    toast_dgemv_
+#define dtrsv_    toast_dtrsv_
+#define dgemm_    toast_dgemm_
+#define dtrsm_    toast_dtrsm_
+
+#define cswap_    toast_cswap_
+#define caxpy_    toast_caxpy_
+#define scasum_   toast_scasum_
+#define icamax_   toast_icamax_
+#define ccopy_    toast_ccopy_
+#define cscal_    toast_cscal_
+#define scnrm2_   toast_scnrm2_
+#define caxpy_    toast_caxpy_
+#define cgemv_    toast_cgemv_
+#define ctrsv_    toast_ctrsv_
+#define cgemm_    toast_cgemm_
+#define ctrsm_    toast_ctrsm_
+#define cgerc_    toast_cgerc_
+#define chemv_    toast_chemv_
+#define cher2_    toast_cher2_
+
+#define zswap_    toast_zswap_
+#define zaxpy_    toast_zaxpy_
+#define dzasum_   toast_dzasum_
+#define izamax_   toast_izamax_
+#define zcopy_    toast_zcopy_
+#define zscal_    toast_zscal_
+#define dznrm2_   toast_dznrm2_
+#define zaxpy_    toast_zaxpy_
+#define zgemv_    toast_zgemv_
+#define ztrsv_    toast_ztrsv_
+#define zgemm_    toast_zgemm_
+#define ztrsm_    toast_ztrsm_
+#define zgerc_    toast_zgerc_
+#define zhemv_    toast_zhemv_
+#define zher2_    toast_zher2_
+
+/* LAPACK */
+#define dlamch_   toast_dlamch_
+#define slamch_   toast_slamch_
+#define xerbla_   toast_xerbla_
+#define lsame_    toast_lsame_
+#define dlacon_   toast_dlacon_
+#define slacon_   toast_slacon_
+#define icmax1_   toast_icmax1_
+#define scsum1_   toast_scsum1_
+#define clacon_   toast_clacon_
+#define dzsum1_   toast_dzsum1_
+#define izmax1_   toast_izmax1_
+#define zlacon_   toast_zlacon_
+
+#else // !TOAST_BLAS_32INT
 
 #if (F77_CALL_C == ADD_)
 /*
@@ -454,5 +545,6 @@
 #define c_fortran_zgssv_ c_fortran_zgssv
 #endif
 
+#endif // !TOAST_BLAS_32INT
 
 #endif /* __SUPERLU_CNAMES */
