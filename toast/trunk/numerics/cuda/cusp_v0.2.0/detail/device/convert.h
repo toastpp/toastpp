7f6e5326493cea44a52693fd3fcdfb953cf17cdd
ucacmas <ucacmas@d6d68596-90e5-4b0b-b9dc-454a62485beb>
2011-10-27 16:28:36 +0000

    Added cusp_v0.2.0 directory

---
+++ toast/trunk/numerics/cuda/cusp_v0.2.0/detail/device/convert.h
@@ -0,0 +1,274 @@
+/*
+ *  Copyright 2008-2009 NVIDIA Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+#pragma once
+
+#include <cusp/format.h>
+#include <cusp/coo_matrix.h>
+
+#include <cusp/detail/device/conversion.h>
+#include <cusp/detail/device/conversion_utils.h>
+
+namespace cusp
+{
+namespace detail
+{
+namespace device
+{
+
+/////////
+// COO //
+/////////
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::csr_format,
+             cusp::coo_format)
+{    cusp::detail::device::csr_to_coo(src, dst);    }
+
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::ell_format,
+             cusp::coo_format)
+{    cusp::detail::device::ell_to_coo(src, dst);    }
+
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::dia_format,
+             cusp::coo_format)
+{    cusp::detail::device::dia_to_coo(src, dst);    }
+
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::hyb_format,
+             cusp::coo_format)
+{    cusp::detail::device::hyb_to_coo(src, dst);    }
+
+/////////
+// CSR //
+/////////
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::coo_format,
+             cusp::csr_format)
+{    cusp::detail::device::coo_to_csr(src, dst);    }
+
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::ell_format,
+             cusp::csr_format)
+{    cusp::detail::device::ell_to_csr(src, dst);    }
+
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::dia_format,
+             cusp::csr_format)
+{    cusp::detail::device::dia_to_csr(src, dst);    }
+
+
+/////////
+// DIA //
+/////////
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::coo_format,
+             cusp::dia_format,
+             const float  max_fill  = 3.0,
+             const size_t alignment = 32)
+{
+    const size_t occupied_diagonals = cusp::detail::device::count_diagonals(src);
+
+    const float threshold  = 1e6; // 1M entries
+    const float size       = float(occupied_diagonals) * float(src.num_rows);
+    const float fill_ratio = size / std::max(1.0f, float(src.num_entries));
+
+    if (max_fill < fill_ratio && size > threshold)
+        throw cusp::format_conversion_exception("dia_matrix fill-in would exceed maximum tolerance");
+
+    cusp::detail::device::coo_to_dia(src, dst, alignment);
+}
+
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::csr_format,
+             cusp::dia_format,
+             const float  max_fill  = 3.0,
+             const size_t alignment = 32)
+{
+    const size_t occupied_diagonals = cusp::detail::device::count_diagonals(src);
+
+    const float threshold  = 1e6; // 1M entries
+    const float size       = float(occupied_diagonals) * float(src.num_rows);
+    const float fill_ratio = size / std::max(1.0f, float(src.num_entries));
+
+    if (max_fill < fill_ratio && size > threshold)
+        throw cusp::format_conversion_exception("dia_matrix fill-in would exceed maximum tolerance");
+
+    cusp::detail::device::csr_to_dia(src, dst, alignment);
+}
+
+/////////
+// ELL //
+/////////
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::coo_format,
+             cusp::ell_format,
+             const float  max_fill  = 3.0,
+             const size_t alignment = 32)
+{
+    const size_t max_entries_per_row = cusp::detail::device::compute_max_entries_per_row(src);    
+
+    const float threshold  = 1e6; // 1M entries
+    const float size       = float(max_entries_per_row) * float(src.num_rows);
+    const float fill_ratio = size / std::max(1.0f, float(src.num_entries));
+
+    if (max_fill < fill_ratio && size > threshold)
+        throw cusp::format_conversion_exception("ell_matrix fill-in would exceed maximum tolerance");
+
+    cusp::detail::device::coo_to_ell(src, dst, max_entries_per_row, alignment);
+}
+
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::csr_format,
+             cusp::ell_format,
+             const float  max_fill  = 3.0,
+             const size_t alignment = 32)
+{
+    const size_t max_entries_per_row = cusp::detail::device::compute_max_entries_per_row(src);
+    
+    const float threshold  = 1e6; // 1M entries
+    const float size       = float(max_entries_per_row) * float(src.num_rows);
+    const float fill_ratio = size / std::max(1.0f, float(src.num_entries));
+
+    if (max_fill < fill_ratio && size > threshold)
+        throw cusp::format_conversion_exception("ell_matrix fill-in would exceed maximum tolerance");
+
+    cusp::detail::device::csr_to_ell(src, dst, max_entries_per_row, alignment);
+}
+
+
+/////////
+// HYB //
+/////////
+
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::coo_format,
+             cusp::hyb_format,
+             const float  relative_speed      = 3.0,
+             const size_t breakeven_threshold = 4096)
+{
+    const size_t num_entries_per_row = cusp::detail::device::compute_optimal_entries_per_row(src, relative_speed, breakeven_threshold);
+    cusp::detail::device::coo_to_hyb(src, dst, num_entries_per_row);
+}
+
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::csr_format,
+             cusp::hyb_format,
+             const float  relative_speed      = 3.0,
+             const size_t breakeven_threshold = 4096)
+{
+    const size_t num_entries_per_row = cusp::detail::device::compute_optimal_entries_per_row(src, relative_speed, breakeven_threshold);
+    cusp::detail::device::csr_to_hyb(src, dst, num_entries_per_row);
+}
+
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::ell_format,
+             cusp::hyb_format)
+{
+    cusp::detail::device::ell_to_hyb(src, dst);
+}
+
+///////////
+// Array //
+///////////
+
+template <typename Matrix1, typename Matrix2, typename MatrixFormat1>
+void convert(const Matrix1& src, Matrix2& dst,
+             MatrixFormat1,
+             cusp::array2d_format)
+{
+    // transfer to host, convert on host, and transfer back to device
+    typedef typename Matrix1::container SourceContainerType;
+    typedef typename Matrix2::container DestinationContainerType;
+    typedef typename DestinationContainerType::template rebind<cusp::host_memory>::type HostDestinationContainerType;
+    typedef typename SourceContainerType::template      rebind<cusp::host_memory>::type HostSourceContainerType;
+
+    HostSourceContainerType tmp1(src);
+
+    HostDestinationContainerType tmp2;
+
+    cusp::detail::host::convert(tmp1, tmp2);
+
+    cusp::copy(tmp2, dst);
+}
+
+template <typename Matrix1, typename Matrix2, typename MatrixFormat2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::array2d_format,
+             MatrixFormat2)
+{
+    // transfer to host, convert on host, and transfer back to device
+    typedef typename Matrix1::container SourceContainerType;
+    typedef typename Matrix2::container DestinationContainerType;
+    typedef typename DestinationContainerType::template rebind<cusp::host_memory>::type HostDestinationContainerType;
+    typedef typename SourceContainerType::template      rebind<cusp::host_memory>::type HostSourceContainerType;
+
+    HostSourceContainerType tmp1(src);
+
+    HostDestinationContainerType tmp2;
+
+    cusp::detail::host::convert(tmp1, tmp2);
+
+    cusp::copy(tmp2, dst);
+}
+
+/////////////////////////////
+// Sparse->Sparse Fallback //
+/////////////////////////////
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst,
+             cusp::sparse_format,
+             cusp::sparse_format)
+{
+   typedef typename Matrix1::index_type IndexType;
+   typedef typename Matrix1::value_type ValueType;
+
+   // convert src -> coo_matrix -> dst
+   cusp::coo_matrix<IndexType, ValueType, cusp::device_memory> tmp;
+   cusp::convert(src, tmp);
+   cusp::convert(tmp, dst);
+}
+
+/////////////////
+// Entry Point //
+/////////////////
+template <typename Matrix1, typename Matrix2>
+void convert(const Matrix1& src, Matrix2& dst)
+{
+    cusp::detail::device::convert(src, dst,
+            typename Matrix1::format(),
+            typename Matrix2::format());
+}
+            
+} // end namespace device
+} // end namespace detail
+} // end namespace cusp
+
