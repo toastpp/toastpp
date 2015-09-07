877939e5b97f3aa2fa8026925d5e0b16092c737f
apogee <mschweiger@users.noreply.github.com>
2015-09-07 15:58:04 +0000

    Gmsh importer now understands 2D meshes consisting of 3-noded triangles

--- script/matlab/utilities/toastReadGmshMesh.m
+++ script/matlab/utilities/toastReadGmshMesh.m
@@ -5,22 +5,40 @@ function mesh = toastReadGmshMesh (fname)
 
 % Read the file, using the Gmsh-provided Matlab converter
 gmsh = load_gmsh(fname);
+eltp = 0;
 
 % Extract vertices
 vtx = gmsh.POS;
 
-% Look only for tetrahedral elements
+% Look for tetrahedral elements
 %tet_idx = find(gmsh.TETS(:,5));
-tets = gmsh.TETS(1:gmsh.nbTets,:);
+els = gmsh.TETS(1:gmsh.nbTets,:);
+if (~isempty(els))
+    idx = els(:,1:4);
+    eltp = 3;
+end
+
+% Look for triangular elements
+if eltp == 0
+    els = gmsh.TRIANGLES(1:gmsh.nbTriangles,:);
+    if (~isempty(els))
+        idx = els(:,1:3);
+        eltp = 15;
+    end
+end
+
+if eltp == 0
+    error('Gmsh element type not supported by toast importer');
+end
 
 % Save region labels
-elreg = tets(:,5);
-tets = tets(:,1:4);
+elreg = els(:,end);
+
 
 % Convert to toast mesh
-nel = size(tets,1);
-eltp = ones(nel,1)*3; % "4-noded tets"
-mesh = toastMesh(vtx, tets, eltp);
+nel = size(idx,1);
+eltp = ones(nel,1)*eltp;
+mesh = toastMesh(vtx, idx, eltp);
 
 % Set region labels
 mesh.Region(elreg);
