246582647ecbe0565e0d480f1c5d2045239e762c
Martin Schweiger <M.Schweiger@ucl.ac.uk>
2016-11-08 16:45:05 +0000

    Matlab bindings: fixed toastElement/Mat for BndF and BndFF

--- src/matlab2/mtElement.cc
+++ src/matlab2/mtElement.cc
@@ -169,6 +169,7 @@ void MatlabToast::ElMat (int nlhs, mxArray *plhs[], int nrhs,
 	pr = mxGetPr(elmat);
 	for (i = 0; i < nnd; i++)
 	    pr[i] = pel->IntF(i);
+
     } else if (!strcmp(cbuf, "FF")) {
 	elmat = mxCreateDoubleMatrix (nnd, nnd, mxREAL);
 	pr = mxGetPr(elmat);
@@ -177,6 +178,7 @@ void MatlabToast::ElMat (int nlhs, mxArray *plhs[], int nrhs,
 	    for (j = 0; j < i; j++)
 		pr[i*nnd+j] = pr[j*nnd+i] = pel->IntFF(i,j);
 	}
+
     } else if (!strcmp(cbuf, "FFF")) {
 	mwSize dims[3] = {nnd,nnd,nnd};
 	elmat = mxCreateNumericArray (3, dims, mxDOUBLE_CLASS, mxREAL);
@@ -188,6 +190,7 @@ void MatlabToast::ElMat (int nlhs, mxArray *plhs[], int nrhs,
 		}
 	    }
 	}
+
     } else if (!strcmp(cbuf, "DD")) {
 	elmat = mxCreateDoubleMatrix (nnd, nnd, mxREAL);
 	pr = mxGetPr(elmat);
@@ -196,6 +199,7 @@ void MatlabToast::ElMat (int nlhs, mxArray *plhs[], int nrhs,
 	    for (j = 0; j < i; j++)
 		pr[i*nnd+j] = pr[j*nnd+i] = pel->IntDD(i,j);
 	}
+
     } else if (!strcmp(cbuf, "FD")) {
 	mwSize dims[3] = {nnd, nnd, dim};
 	elmat = mxCreateNumericArray (3, dims, mxDOUBLE_CLASS, mxREAL);
@@ -210,6 +214,7 @@ void MatlabToast::ElMat (int nlhs, mxArray *plhs[], int nrhs,
 		for (k = 0; k < dim; k++)
 		    pr[i + nnd*(j + k*nnd)] = fd[k];
 	    }
+
     } else if (!strcmp(cbuf, "FDD")) {
 	mwSize dims[3] = {nnd, nnd, nnd};
 	elmat = mxCreateNumericArray (3, dims, mxDOUBLE_CLASS, mxREAL);
@@ -218,6 +223,7 @@ void MatlabToast::ElMat (int nlhs, mxArray *plhs[], int nrhs,
 	    for (j = 0; j < nnd; j++)
 		for (k = 0; k < nnd; k++)
 		    pr[i+nnd*(j+nnd*k)] = pel->IntFDD(i,j,k);
+
     } else if (!strcmp(cbuf, "dd")) {
 	mwSize dims[4] = {nnd, dim, nnd, dim};
 	elmat = mxCreateNumericArray (4, dims, mxDOUBLE_CLASS, mxREAL);
@@ -241,6 +247,7 @@ void MatlabToast::ElMat (int nlhs, mxArray *plhs[], int nrhs,
 	        pr[i*nnd+j] = pr[j*nnd+i] = intPFF(i,j);
 	}
 	nprm++;
+	
     } else if (!strcmp(cbuf, "PDD")) {
         elmat = mxCreateDoubleMatrix (nnd, nnd, mxREAL);
 	RVector prm;
@@ -253,17 +260,26 @@ void MatlabToast::ElMat (int nlhs, mxArray *plhs[], int nrhs,
 	        pr[i*nnd+j] = pr[j*nnd+i] = intPDD(i,j);
 	}
 	nprm++;
+	
     } else if (!strcmp(cbuf, "BndF")) {
-	// for now, only integrals over all boundary sides are supported
+	int sd;
+	if (nrhs > 3) sd = (int)mxGetScalar(prhs[3]) -1; // side index
+	else sd = -1;
 	elmat = mxCreateDoubleMatrix (nnd, 1, mxREAL);
 	pr = mxGetPr(elmat);
-	RVector bndintf = pel->BndIntF();
-	for (i = 0; i < pel->nNode(); i++)
-	    pr[i] = bndintf[i];
-
+	if (sd >= 0) { // integral over a single side
+	    for (i = 0; i < nnd; i++) {
+		pr[i] = pel->BndIntFSide(i,sd);
+	    }
+	} else {
+	    RVector bndintf = pel->BndIntF();
+	    for (i = 0; i < nnd; i++)
+		pr[i] = bndintf[i];
+	}
+	
     } else if (!strcmp(cbuf, "BndFF")) {
 	int ii, jj, sd;
-	if (nrhs > nprm) sd = (int)mxGetScalar(prhs[nprm+1]) - 1; // side index
+	if (nrhs > 3) sd = (int)mxGetScalar(prhs[3]) - 1; // side index
 	else sd = -1;
 	elmat = mxCreateDoubleMatrix (nnd, nnd, mxREAL);
 	pr = mxGetPr(elmat);
@@ -290,6 +306,7 @@ void MatlabToast::ElMat (int nlhs, mxArray *plhs[], int nrhs,
 		}
 	    }
 	}
+
     } else if (!strcmp(cbuf, "BndPFF")) {
         int sd;
         if (nrhs > nprm) sd =  (int)mxGetScalar(prhs[nprm+1]) - 1; // side index
